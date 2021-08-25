24a0974ae31e1998188204cddf8c380a7be586af
martin-cummins <martin.cummins@ie.ibm.com>
2021-08-25 16:46:06 +0100

    Make the dest dir on the Travis.

--- scripts/downloadInstaller.sh
+++ scripts/downloadInstaller.sh
@@ -18,5 +18,6 @@ fi
 # IIM
 if [ "$1" = "iim-191-centos-8" ]; then
   echo "Install IIM"
+  mkdir -p /tmp/iim/
   curl -H "X-JFrog-Art-Api: ${ARTIFACTORY_TOKEN}" -k ${ARTIFACTORY_URL}/${ARTIFACTORY_REPO}/SoftwareInstallers/IIM/agent.installer.linux.gtk.x86_64_1.9.1001.20191112_1525.zip -o /tmp/iim/iim_installer.tar.gz
 fi
